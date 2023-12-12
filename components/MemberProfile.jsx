c03ba29c0ccb5947d3bc0d7b8ecb9207899d2c39
Sayantan_627 <27sayantan.pal@gmail.com>
2023-12-12 19:52:08 +0530

    comment display token

--- components/MemberProfile.jsx
+++ components/MemberProfile.jsx
@@ -3,8 +3,8 @@ import { UserButton, currentUser, auth } from "@clerk/nextjs";
 
 const MemberProfile = async () => {
   const user = await currentUser();
-  const { userId } = auth();
-  await fetchOrGenerateTokens(userId);
+  // const { userId } = auth();
+  // await fetchOrGenerateTokens(userId);
   // console.log(userId);
   return (
     <div className="px-4 flex items-center gap-2">
